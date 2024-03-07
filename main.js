c4969bc4ac9dddeb98f499e8869ec111b54c1f59
Bhagy3sh <141747782+Bhagy3sh@users.noreply.github.com>
2024-03-08 01:13:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const sidebar = document.querySelector('#sidebartop')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('#sidebartop')
+    sidebar.style.display = 'none'
+}
